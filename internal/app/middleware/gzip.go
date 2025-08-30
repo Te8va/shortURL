30a40758f98409c50b1c8572c3377511e2e298f0
Te8va <jane14042000@mail.ru>
2025-08-31 00:30:25 +0300

    fix fmt

--- internal/app/middleware/gzip.go
+++ internal/app/middleware/gzip.go
@@ -19,26 +19,26 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 
 // GzipHandle wraps an HTTP handler to transparently handle gzip compression and decompression for requests and responses when supported.
 func GzipHandle(h http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if r.Header.Get("Content-Encoding") == "gzip" {
-            gz, err := gzip.NewReader(r.Body)
-            if err != nil {
-                http.Error(w, "Failed to decompress request", http.StatusBadRequest)
-                return
-            }
-            defer gz.Close()
-            r.Body = io.NopCloser(gz) 
-        }
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-Encoding") == "gzip" {
+			gz, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, "Failed to decompress request", http.StatusBadRequest)
+				return
+			}
+			defer gz.Close()
+			r.Body = io.NopCloser(gz)
+		}
 
-        if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-            w.Header().Set("Content-Encoding", "gzip")
-            gz := gzip.NewWriter(w)
-            defer gz.Close()
-            gw := gzipWriter{ResponseWriter: w, Writer: gz}
-            h.ServeHTTP(gw, r)
-            return
-        }
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Set("Content-Encoding", "gzip")
+			gz := gzip.NewWriter(w)
+			defer gz.Close()
+			gw := gzipWriter{ResponseWriter: w, Writer: gz}
+			h.ServeHTTP(gw, r)
+			return
+		}
 
-        h.ServeHTTP(w, r)
-    })
+		h.ServeHTTP(w, r)
+	})
 }
